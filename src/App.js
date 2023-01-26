2553de2cd8023f7e3e9d020e98b0424387db9be6
TomE <tommevans210@gmail.com>
2023-01-26 12:39:41 +0700

    updated site message

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>it's me, tom!</p>
+        <p>updated site</p>
       </header>
     </div>
   );
