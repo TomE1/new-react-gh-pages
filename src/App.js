e1aad31776b868d16c80d552dcd5c55ae6dadd4a
TomE <tommevans210@gmail.com>
2023-01-27 13:29:59 +0700

    added page links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Profile from './Pages/Profile';
+import ErrorPage from './Pages/ErrorPage';
 
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/profile">Profile</Link>
+      </nav>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/profile' element={<Profile />} />
+        <Route path='*' element={<ErrorPage />} />
       </Routes>
     </Router>
   );
---
+++ src/Pages/ErrorPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ErrorPage() {
+  return <div>ERROR >>>>> YOU SHOULDN'T BE HERE BRO!</div>;
+}
+
+export default ErrorPage;
