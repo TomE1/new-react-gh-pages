695a634ccc097962e654a2315d0fa49bc54b872a
TomE <tommevans210@gmail.com>
2023-01-27 09:58:38 +0700

    rebuilt

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-   <p>changed name of site</p>
-
+        <p>rebuilt</p>
       </header>
     </div>
   );
