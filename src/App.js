1c63df269bbb7cf99ad1979a580a416a53e2531b
TomE <tommevans210@gmail.com>
2023-01-27 10:02:05 +0700

    rebuilt a second time

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>rebuilt</p>
+        <p>rebuilt a second time</p>
       </header>
     </div>
   );
